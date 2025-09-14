837fdebbdc5637dfd63d8c91ece3b12c1dbe3951
kanatbekova telegey <telegey@MacBook-Air-kanatbekova.local>
2025-09-14 15:42:44 +0600

    no

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,33 @@
-import type { ReactElement } from "react";
-import Footer from "./components/layout/footer/Footer";
-import Header from "./components/layout/header/Header";
-import Main from "./components/pages/main/Main";
-import { Route, Routes } from "react-router-dom";
+import type { ReactElement } from 'react'
+import Footer from './components/layout/footer/Footer'
+import Header from './components/layout/header/Header'
+import Main from './components/pages/main/Main'
+import { Route, Routes } from 'react-router-dom'
 
 interface IRouter {
-  id: number;
-  path: string;
-  element: ReactElement;
+	id: number
+	path: string
+	element: ReactElement
 }
 const App = () => {
-  const router: IRouter[] = [
-    {
-      id: 1,
-      path: "/",
-      element: <Main />,
-    },
-  ];
-  return (
-    <div className="app">
-      <Header />
-      <Routes>
-        {router.map((el) => (
-          <Route path={el.path} element={el.element} key={el.id} />
-        ))}
-      </Routes>
-      <Footer />
-    </div>
-  );
-};
+	const router: IRouter[] = [
+		{
+			id: 1,
+			path: '/',
+			element: <Main />,
+		},
+	]
+	return (
+		<div className='app'>
+			<Header />
+			<Routes>
+				{router.map((el) => (
+					<Route path={el.path} element={el.element} key={el.id} />
+				))}
+			</Routes>
+			<Footer />
+		</div>
+	)
+}
 
-export default App;
+export default App
